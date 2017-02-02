4f3ea4ace3fdbb77f0ae03a2386c4739223115ba
Algain <miztavallavan@gmail.com>
2017-02-02 16:53:59 +0800

    Add files via upload

---
+++ src/test/java/ToolsQA/Mavendemoproject/viewpageTest.java
@@ -0,0 +1,21 @@
+package ToolsQA.Mavendemoproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class viewpageTest {
+
+	@Test
+	public void TestFireFox(){
+	System.out.println("Testing");
+	
+	System.setProperty("webdriver.gecko.driver", "./geckodriver\\geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+	//WebDriver driver=new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.get("http://admin.ehalal.com/supplier/login");
+	driver.quit();
+	}
+}
