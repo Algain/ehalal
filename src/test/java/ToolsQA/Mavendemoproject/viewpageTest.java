6ab2a469ca301cda27595371b5887dd2634da5ff
Algain <miztavallavan@gmail.com>
2017-02-03 15:26:38 +0800

    Update viewpageTest.java

--- src/test/java/ToolsQA/Mavendemoproject/viewpageTest.java
+++ src/test/java/ToolsQA/Mavendemoproject/viewpageTest.java
@@ -1,6 +1,8 @@
 package ToolsQA.Mavendemoproject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -8,8 +10,11 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 public class viewpageTest {
 
 	@Test
-	public void TestFireFox(){
-		
+	public void TestFireFox() throws InterruptedException{
+	// turn off htmlunit warnings
+	java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.OFF);
+	java.util.logging.Logger.getLogger("org.apache.http").setLevel(java.util.logging.Level.OFF);
+
 	System.out.println("Testing");
 	
 	//System.setProperty("webdriver.gecko.driver", "./geckodriver\\geckodriver.exe");
@@ -21,6 +26,24 @@ public class viewpageTest {
 	WebDriver driver = new HtmlUnitDriver();
 	driver.get("http://admin.ehalal.com/supplier/login");
 	System.out.println("Title of the page "+ driver.getTitle());
+	
+	WebElement username = driver.findElement(By.id("username"));
+	username.sendKeys("algain@theflyingcoders.com");
+	
+	WebElement password = driver.findElement(By.id("password"));
+	password.sendKeys("Abcd1234");
+	
+	Thread.sleep(2000);
+	
+	WebElement login = driver.findElement(By.id("login_submit"));
+	login.click();
+	Thread.sleep(2000);
+	
+	System.out.println("Login Successful");
+	
+	driver.get("http://admin.ehalal.com/supplier/logout");
+	
+	System.out.println("Log Off");
 	driver.quit();
 	}
 }
