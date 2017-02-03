5d0ca795e1079f1aeb0d87fecdb1103afccc91cd
Algain <miztavallavan@gmail.com>
2017-02-03 14:26:21 +0800

    Update viewpageTest.java

--- src/test/java/ToolsQA/Mavendemoproject/viewpageTest.java
+++ src/test/java/ToolsQA/Mavendemoproject/viewpageTest.java
@@ -3,19 +3,24 @@ package ToolsQA.Mavendemoproject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class viewpageTest {
 
 	@Test
 	public void TestFireFox(){
+		
 	System.out.println("Testing");
 	
-	System.setProperty("webdriver.gecko.driver", "F:\\geckodriver\\geckodriver.exe");
-	WebDriver driver=new FirefoxDriver();
+	//System.setProperty("webdriver.gecko.driver", "./geckodriver\\geckodriver.exe");
 	//WebDriver driver=new FirefoxDriver();
-	driver.manage().window().maximize();
+	//WebDriver driver=new FirefoxDriver();
+	//driver.manage().window().maximize();
+	
+	//Run Headless
+	WebDriver driver = new HtmlUnitDriver();
 	driver.get("http://admin.ehalal.com/supplier/login");
+	System.out.println("Title of the page "+ driver.getTitle());
 	driver.quit();
 	}
 }
